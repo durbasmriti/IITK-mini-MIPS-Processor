8371c311c518c13bdb0e890c848aa7e8c583dea8
DhritiBarnwal <dhritib23@iitk.ac.in>
2025-04-19 16:02:33 +0530

    Update insertion_sort.s

--- insertion_sort.s
+++ insertion_sort.s
@@ -9,44 +9,41 @@ length:     .word 10
 
 main:
     la   $t0, numbers      # $t0 = base address of numbers
-    lw   $t1, length       # $t1 = length of array
+    lw   $t1, length       
 
-    addi $t2, $zero, 1     # $t2 = firstUnsortedIndex = 1
+    addi $t2, $zero, 1     
 
 outer_loop:
-    bge  $t2, $t1, end     # if firstUnsortedIndex >= length, end loop
+    bge  $t2, $t1, end     
 
-    # elementToInsert = numbers[firstUnsortedIndex]
-    sll  $t3, $t2, 2       # $t3 = firstUnsortedIndex * 4 (word offset)
-    add  $t4, $t0, $t3     # $t4 = &numbers[firstUnsortedIndex]
-    lw   $t5, 0($t4)       # $t5 = elementToInsert
+    sll  $t3, $t2, 2       
+    add  $t4, $t0, $t3     
+    lw   $t5, 0($t4)       
 
-    addi $t6, $t2, -1      # $t6 = testIndex = firstUnsortedIndex - 1
+    addi $t6, $t2, -1      
 
 inner_loop:
-    bltz $t6, insert       # if testIndex < 0, exit inner loop
+    bltz $t6, insert       
 
-    sll  $t7, $t6, 2       # $t7 = testIndex * 4
-    add  $t8, $t0, $t7     # $t8 = &numbers[testIndex]
-    lw   $t9, 0($t8)       # $t9 = numbers[testIndex]
+    sll  $t7, $t6, 2       
+    add  $t8, $t0, $t7     
+    lw   $t9, 0($t8)       
 
-    ble  $t9, $t5, insert  # if numbers[testIndex] <= elementToInsert, exit inner loop
+    ble  $t9, $t5, insert  
 
-    # numbers[testIndex + 1] = numbers[testIndex]
     sw   $t9, 4($t8)
 
-    addi $t6, $t6, -1      # testIndex--
+    addi $t6, $t6, -1      
     j    inner_loop
 
 insert:
-    sll  $t7, $t6, 2       # $t7 = testIndex * 4
-    add  $t8, $t0, $t7     # $t8 = &numbers[testIndex]
-    sw   $t5, 4($t8)       # numbers[testIndex + 1] = elementToInsert
+    sll  $t7, $t6, 2       
+    add  $t8, $t0, $t7     
+    sw   $t5, 4($t8)       
 
-    addi $t2, $t2, 1       # firstUnsortedIndex++
+    addi $t2, $t2, 1       
     j    outer_loop
 
 end:
-    # (Optional) Add code here to print the sorted array if desired
     li   $v0, 10           # exit
     syscall
